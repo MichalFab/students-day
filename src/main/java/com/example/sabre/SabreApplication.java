2ed1f39d7a9dfe370771577de59efe5f8acc76f7
Michal Fabjanski <michal.fabjanski@sabre.com>
2019-03-13 17:11:08 +0100

    Hello aplication

---
+++ src/main/java/com/example/sabre/SabreApplication.java
@@ -0,0 +1,13 @@
+package com.example.sabre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SabreApplication.class, args);
+    }
+
+}
